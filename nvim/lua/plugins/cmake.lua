18059151ead1f211c4926a85c2c2f795d938c4f0
DiamondArrowTheLizard <diamond1arrow@gmail.com>
2025-12-06 05:10:51 +0700

    added variable to generate compile_commands.json

--- nvim/lua/plugins/cmake.lua
+++ nvim/lua/plugins/cmake.lua
@@ -1,6 +1,7 @@
 return {
     'cdelledonne/vim-cmake',
     config = function()
+        vim.g.cmake_link_compile_commands = 1
         vim.keymap.set('n', '<leader><CR>', '<CMD>lua require("FTerm").toggle()<CR>')
         vim.keymap.set('n', '<leader>cmg', ':CMakeGenerate<CR>')
         vim.keymap.set('n', '<leader>cmb', ':CMakeBuild<CR>')
